2413ad81dcd77b5b5fd07bcc7fee62845cbbcc7d
Wil <wilhelmjanmapogi@gmail.com>
2022-04-16 10:11:35 +0800

    Add get and post on item create form

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const Item = require("../models/Item.js");
 const Category = require("../models/Category");
 
+const { body, validationResult } = require("express-validator");
+
 // Get the full list of items
 exports.getItemList = async (req, res, next) => {
   try {
@@ -49,4 +51,50 @@ exports.getCreateItem = async (req, res, next) => {
     return next(err);
   }
 };
-exports.postCreateItem = (req, res, next) => {};
+
+exports.postCreateItem = [
+  body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Invalid Name")
+    .isAlphanumeric()
+    .withMessage("Name must only use alphanumeric characters"),
+  body("description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage(" Description Must not be empty"),
+  body("price", "Use numeric characters only").trim().isNumeric().escape(),
+  body("stockCode")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Stock Code must not be empty"),
+  body("category").trim().escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      stockCode: req.body.stockCode,
+      category: req.body.category,
+    });
+    try {
+      if (!errors.isEmpty()) {
+        const categories = await Category.find({}).exec();
+        res.render("itemForm", {
+          title: "Create Item (Err)",
+          errors: errors.array(),
+          categories,
+        });
+      }
+      await item.save();
+      return res.redirect(item.url);
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
